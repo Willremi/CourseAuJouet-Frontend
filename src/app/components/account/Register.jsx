3269a57b7de3e39cbd84b1bb5900c6dbfbcd7666
Adrien Boudet <adrien.boudet1@gmail.com>
2021-11-02 15:25:50 +0100

    mise en forme

--- src/app/components/account/Register.jsx
+++ src/app/components/account/Register.jsx
@@ -1,6 +1,21 @@
 const Register = () => {
     return (
-        <h1>ceci est la page Register</h1>
+        <div className='bg-white p-4 rounded-md shadow max-w-md w-full space-y-8 py-12 px-4 sm:px-6 lg:px-8'>
+            <div>
+                <div className='flex justify-center'>
+                    <img
+                        className="h-12 w-auto sm:h-10 cursor-pointer"
+                        src="https://insy2s.com/insy2s/images/Logo-insy2s-INLINE-2021.svg"
+                        alt=""
+                        width={200}
+                        height={60}
+                    />
+                </div>
+                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-800">
+                ceci est la page Register
+                </h2>
+            </div>
+        </div>
     )
 }
 
