70aa7d460a7bd3bd97408999921fab810d9be2a8
Maxence Ramael <maxence.ramael@gmail.com>
2021-11-08 16:15:16 +0100

    Début hooks

--- src/app/shared/components/pdt-components/ProduitCard.jsx
+++ src/app/shared/components/pdt-components/ProduitCard.jsx
@@ -1,16 +1,34 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
+
+
 
 function ProduitCard() {
+  let data = {
+    img: "monop.jpg",
+    name: "Monopoly ville de Nantes",
+    price: "30,99",
+  };
+  const [produit, setproduit] = useState(data);
+  console.log(useState(data));
   return (
     <>
       <div className="w-48 h-80 border border-gray rounded-md ml-8 shadow-md ">
         <div className="flex w-48 h-48 border border-gray rounded-md shadow-xl">
-          <img src="monop.jpg" alt="im pdt" className="w-40 h-40 flex  self-center mx-auto border border-gray-200 "/>
+          <img
+            src={data.img}
+            alt="im pdt"
+            className="w-40 h-40 flex  self-center mx-auto border border-gray-200 "
+          />
         </div>
         <div className="mt-2 flex flex-col space-y-1">
-          <h2 className="ml-2"><b>Monopoly</b></h2>
-          <h2  className="ml-2"><b>29,99 €</b></h2>
-
+          <h2 className="ml-2">
+            <b>{data.name}</b>
+          </h2>
+          <h2 className="ml-2">
+            <b>{data.price} €</b>
+          </h2>
         </div>
       </div>
     </>
