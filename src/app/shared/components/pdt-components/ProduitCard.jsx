86bd2e3125e4608de3c4d143ed58c09b97162227
Maxence Ramael <maxence.ramael@gmail.com>
2021-11-08 15:32:18 +0100

    suite card pdt

--- src/app/shared/components/pdt-components/ProduitCard.jsx
+++ src/app/shared/components/pdt-components/ProduitCard.jsx
@@ -3,13 +3,13 @@ import React from "react";
 function ProduitCard() {
   return (
     <>
-      <div className="w-48 h-80 border border-gray rounded-md ml-8">
-        <div className="w-48 h-48 border border-black rounded-md bg-gray-300">
-          <img src="" alt="im pdt" />
+      <div className="w-48 h-80 border border-gray rounded-md ml-8 shadow-md ">
+        <div className="flex w-48 h-48 border border-gray rounded-md shadow-xl">
+          <img src="monop.jpg" alt="im pdt" className="w-40 h-40 flex  self-center mx-auto border border-gray-200 "/>
         </div>
-        <div className="mt-2 flex flex-col space-y-2">
-          <h2>Monopoly</h2>
-          <h2>29,99 €</h2>
+        <div className="mt-2 flex flex-col space-y-1">
+          <h2 className="ml-2"><b>Monopoly</b></h2>
+          <h2  className="ml-2"><b>29,99 €</b></h2>
 
         </div>
       </div>
