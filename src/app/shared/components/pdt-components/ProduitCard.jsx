1cdaf7e8f2129a0058b3b233e9140b483460efa4
Maxence Ramael <maxence.ramael@gmail.com>
2021-11-08 15:00:56 +0100

    Card pdt v1

--- src/app/shared/components/pdt-components/ProduitCard.jsx
+++ src/app/shared/components/pdt-components/ProduitCard.jsx
@@ -3,7 +3,16 @@ import React from "react";
 function ProduitCard() {
   return (
     <>
-     
+      <div className="w-48 h-80 border border-gray rounded-md ml-8">
+        <div className="w-48 h-48 border border-black rounded-md bg-gray-300">
+          <img src="" alt="im pdt" />
+        </div>
+        <div className="mt-2 flex flex-col space-y-2">
+          <h2>Monopoly</h2>
+          <h2>29,99 €</h2>
+
+        </div>
+      </div>
     </>
   );
 }
