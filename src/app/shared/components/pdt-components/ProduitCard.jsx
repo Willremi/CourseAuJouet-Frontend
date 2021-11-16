e929593dbf1353795c335266dd38b049388a411d
Maxence Ramael <maxence.ramael@gmail.com>
2021-11-16 14:22:43 +0100

    map composant pdt

--- src/app/shared/components/pdt-components/ProduitCard.jsx
+++ src/app/shared/components/pdt-components/ProduitCard.jsx
@@ -31,6 +31,26 @@ function ProduitCard() {
   console.log(useState());
   return (
     <>
+    {/* {data.map((pdt) => {
+      <div className="w-48 h-80 border border-gray rounded-md ml-8 shadow-md ">
+      <div className="flex w-48 h-48 border border-gray rounded-md shadow-xl">
+        <img
+          key={pdt.id}
+          src={pdt.img}
+          alt="im pdt"
+          className="w-40 h-40 flex  self-center mx-auto border border-gray-200 "
+        />
+      </div>
+      <div className="mt-2 flex flex-col space-y-1">
+        <h2 className="ml-2" key={pdt.id}>
+          <b>{pdt.name}</b>
+        </h2>
+        <h2 className="ml-2" key={pdt.id}>
+          <b>{pdt.price} €</b>
+        </h2>
+      </div>
+    </div>
+    })} */}
       <div className="w-48 h-80 border border-gray rounded-md ml-8 shadow-md ">
         <div className="flex w-48 h-48 border border-gray rounded-md shadow-xl">
           <img
