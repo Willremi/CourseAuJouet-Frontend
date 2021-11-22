ec96b31d4a0c6149c6686a04ac8d42e425066e1e
Adrien Boudet <adrien.boudet1@gmail.com>
2021-11-22 17:01:05 +0100

    tailwind improvements

--- src/app/components/layouts/carousel/Carousel.jsx
+++ src/app/components/layouts/carousel/Carousel.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { Transition } from '@headlessui/react'
 import { CarouselData } from "./CarouselData";
 import {FaChevronLeft,FaChevronRight} from 'react-icons/fa'
+import { Link } from "react-router-dom";
 
 const Carousel = () => {
     const [current, setCurrent] = useState(0)
@@ -26,8 +28,16 @@ const Carousel = () => {
            <FaChevronRight className="absolute inset-y-1/2 right-7 font text-5xl text-black z-20 cursor-pointer select-none opacity-40" onClick={nextSlide}/>
             {CarouselData.map((slide,index) => {
                 return (
-                    <div className={index === current ? 'slide active ' : 'slide'} key={index}>
-                        {index === current && (<img src={slide.image} alt={slide.alt} className="w-full h-3/4" />)}
+                    <div className={index === current ? "opacity-1 transition duration-1000 hover:opacity-90" : "opacity-50 transition duration-1000 ease-in"} key={index}>
+                        {index === current && (<img src={slide.image} alt={slide.alt} className="min-w-full h-3/4 rounded-b-xl" />)}
+                        <div className="absolute top-10 left-10 ">
+                            { index === current && slide.text}
+                            <Link to={slide.link} >
+                                <button className="bg-yellow-400 rounded-xl p-6">
+                                    Voir plus
+                                </button>
+                            </Link>
+                        </div>
                         
                     </div>
                 )
--- src/app/components/layouts/carousel/CarouselData.jsx
+++ src/app/components/layouts/carousel/CarouselData.jsx
@@ -1,14 +1,20 @@
 export const CarouselData = [
     {
         image: "https://images.unsplash.com/photo-1581277868137-9dc9f38abc2c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1471&q=80",
-        alt: "ceci est un alt"
+        alt: "ceci est un alt",
+        text: "surprenez les enfants à noël",
+        link: "#"
     },
     {
         image: 'https://images.unsplash.com/photo-1580477667929-3ef27c684b7a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80',
-        alt: "ceci est un alt"
+        alt: "ceci est un alt",
+        text: "surprenez les enfants à noël",
+        link: "#"
     },
     {
         image: 'https://images.unsplash.com/photo-1579461240202-74f8b456af3d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80',
-        alt: "ceci est un alt"
+        alt: "ceci est un alt",
+        text: "surprenez les enfants à noël",
+        link: "#"
     }
 ];
