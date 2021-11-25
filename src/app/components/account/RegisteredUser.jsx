488a12d6d770e963385b6eb82c79551aac520715
Maxence Ramael <maxence.ramael@gmail.com>
2021-11-25 16:36:43 +0100

    Modal fonctionnel sans style

--- src/app/components/account/RegisteredUser.jsx
+++ src/app/components/account/RegisteredUser.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { verifyUser } from "../../api/backend/account";
-import ModalAuth from "../../shared/components/modal-components/modal";
+import ModalAuth from "../../shared/components/modal-components/ModalAuth";
+import NewProduct from "../homepage/NewProduct";
+
 
 const RegisteredUser = () => {
   
@@ -16,8 +18,8 @@ const RegisteredUser = () => {
   
   return (
     <div className="container">
-     Ceci est un modal de confirmation 
-     <ModalAuth />
+      <NewProduct />
+ <ModalAuth />
     </div> 
   );
 };
