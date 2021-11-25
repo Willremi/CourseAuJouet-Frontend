838a7446a8d3d135913b9cac2756b4252d83ae6f
Adrien Boudet <adrien.boudet1@gmail.com>
2021-11-25 16:37:41 +0100

    premerge

--- src/app/components/layouts/carousel/Carousel.jsx
+++ src/app/components/layouts/carousel/Carousel.jsx
@@ -6,16 +6,14 @@ import { Link } from "react-router-dom";
 const Carousel = () => {
     const [current, setCurrent] = useState(0)
     const length = CarouselData.length
-
+    
     const nextSlide = () => {
         setCurrent(current === length -1 ? 0 : current + 1)
     }
-    
+
     const prevSlide = () => {
         setCurrent(current === 0 ? length -1 : current - 1)
     }
-
-    console.log(current)
 
     if (!Array.isArray(CarouselData) || length <= 0) {
         return null;
@@ -23,29 +21,24 @@ const Carousel = () => {
 
     return(
         <section className="flex w-screen">
-
             {/* mapage des données */}
                 {CarouselData.map((slide,index) => {
                     return (
                         <div className="h-screen w-full">
-
-                            {/* TODO: Trouver d'où vient l'overflow-x et la barre de scroll-x
-                                
-                            */}
                             {/* ceci est l'image */}
                             <div className={index === current 
                                 ? "relative opacity-1 transition duration-1000 hover:opacity-90 h-3/4 w-screen rounded-b-xl bg-center bg-cover sm:h-3/4" 
                                 : "opacity-40 transition duration-1000 ease-in hidden"}key={index} style={{ backgroundImage: `url(${slide.image})` }}>
 
                                     {/* les deux cheuvrons servant à manipuler le carousel */}
-                                    <FaChevronLeft className="absolute inset-y-1/2 left-7 font text-5xl text-black z-20 cursor-pointer select-none opacity-40" onClick={prevSlide} />
-                                    <FaChevronRight className="absolute inset-y-1/2 right-7 font text-5xl text-black z-20 cursor-pointer select-none opacity-40" onClick={nextSlide}/>
+                                    <FaChevronLeft className="absolute inset-y-1/2 left-6 sm:left-1 md:left-3 font text-5xl text-black z-20 cursor-pointer select-none opacity-40" onClick={prevSlide} />
+                                    <FaChevronRight className="absolute inset-y-1/2 right-6 sm:right-1 md:right-3 font text-5xl text-black z-20 cursor-pointer select-none opacity-40" onClick={nextSlide}/>
                                 
                                 {/* la div contenant les infos et le lien correspondant à l'image */}
                                 <div className={index === current 
-                                    ? " opacity-1 transition duration-1000 absolute sm:static sm:text-center sm:p-10 top-20 left-20  text-center" 
+                                    ? "opacity-1 transition duration-1000 absolute sm:static sm:p-10 top-20 left-20 text-center" 
                                     : "opacity-0"} key={index}>
-                                    <p className="text-3xl  sm:text-blue-400 text-yellow-400 text-center">
+                                    <p className="text-3xl sm:text-blue-400 text-yellow-400 text-center">
                                     { index === current && slide.text}
                                     </p>
                                     <Link to={slide.link} >  
