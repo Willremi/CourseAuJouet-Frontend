febfbe5072952783c6dc63e03952cf4aea18fc85
Adrien Boudet <adrien.boudet1@gmail.com>
2021-12-10 17:00:03 +0100

    amelioration du carousel en cours

--- src/app/components/layouts/carousel/Carousel.jsx
+++ src/app/components/layouts/carousel/Carousel.jsx
@@ -3,56 +3,76 @@ import { CarouselData } from "./CarouselData";
 import {FaChevronLeft,FaChevronRight} from 'react-icons/fa'
 import { Link } from "react-router-dom";
 
-const Carousel = () => {
-    const [current, setCurrent] = useState(0)
-    const length = CarouselData.length
-    
+const Carousel = (props) => {
+    const getWidth = () => window.innerWidth
+
+    const [ state, setState ] = useState({
+        activeIndex: 0,
+        translate: 0,
+        transition: 0.45
+    })
+
+    const { translate, transition, activeIndex} = state
+
     const nextSlide = () => {
-        setCurrent(current === length -1 ? 0 : current + 1)
+        if (activeIndex === props.slides.length - 1) {
+          return setState({
+            ...state,
+            translate: 0,
+            activeIndex: 0
+          })
+        }
+
+        setState({
+            ...state,
+            activeIndex: activeIndex + 1,
+            translate: (activeIndex + 1) * getWidth()
+        })
     }
+
 
     const prevSlide = () => {
-        setCurrent(current === 0 ? length -1 : current - 1)
-    }
+        if (activeIndex === 0) {
+          return setState({
+            ...state,
+            translate: (props.slides.length - 1) * getWidth(),
+            activeIndex: props.slides.length - 1
+          })
+        }
+    
+        setState({
+          ...state,
+          activeIndex: activeIndex - 1,
+          translate: (activeIndex - 1) * getWidth()
+        })
+      }
 
-    if (!Array.isArray(CarouselData) || length <= 0) {
-        return null;
-    }
+    return (
+    <div css={SliderCSS}>
+        <SliderContent
+        translate={translate}
+        transition={transition}
+        width={getWidth() * props.slides.length}
+        >
+        {props.slides.map((slide, i) => (
+            <Slide key={slide + i} content={slide} />
+        ))}
+        </SliderContent>
 
-    return(
-        <section>
-            {/* mapage des données */}
-                {CarouselData.map((slide,index) => {
-                    return (
-                        <div key={index} className={`${index === current ? "w-screen h-screen-40 md:h-screen-40 sm:h-screen-50 relative" : "hidden"}`}>
-                            {/* ceci est l'image */}
-                            <Link to={slide.link} >  
-                            <img src={slide.image} alt="produit" 
-                            className="w-full h-full object-bottom object-cover"/>
-                            </Link>
-                                    {/* les deux cheuvrons servant à manipuler le carousel */}
-                                    <FaChevronLeft className="absolute inset-y-1/2 left-6 sm:left-1 md:left-3 font text-5xl text-black z-20 cursor-pointer select-none opacity-40" onClick={prevSlide} />
-                                    <FaChevronRight className="absolute inset-y-1/2 right-6 sm:right-1 md:right-3 font text-5xl text-black z-20 cursor-pointer select-none opacity-40" onClick={nextSlide}/>
-                                
-                                {/* la div contenant les infos et le lien correspondant à l'image */}
-                                <div className={index === current 
-                                    ? "opacity-1 transition duration-1000 absolute sm:static sm:p-10 sm:hidden top-20 left-20 text-center" 
-                                    : "opacity-0"} key={index}>
-                                    <p className="text-3xl sm:text-blue-400 text-yellow-400 text-center">
-                                    { index === current && slide.text}
-                                    </p>
-                                    <Link to={slide.link} >  
-                                        <button className="sm:bg-nav-blueClar bg-yellow-400 hover:bg-yellow-500 rounded-full px-7 py-2 mt-2 sm:mt-4">
-                                            Voir plus
-                                        </button>
-                                    </Link>
-                                </div>
-                            </div>  
-                       
+        <Arrow direction="left" handleClick={prevSlide} />
+        <Arrow direction="right" handleClick={nextSlide} />
 
-                    )
-                })}
-       </section>
+        <Dots slides={props.slides} activeIndex={activeIndex} />
+    </div>
     )
-};
+}
+
+const SliderCSS = css`
+    position: relative;
+    height: 100vh;
+    width: 100vw;
+    margin: 0 auto;
+    overflow: hidden;
+`
+
 export default Carousel
