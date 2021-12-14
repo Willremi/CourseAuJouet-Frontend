98720ccf505e1fbd4b9c05f33c9e546ada07bcaf
Adrien Boudet <adrien.boudet1@gmail.com>
2021-12-14 13:10:10 +0100

    ajout de l'autoplay

--- src/app/components/layouts/carousel/Carousel.jsx
+++ src/app/components/layouts/carousel/Carousel.jsx
@@ -1,65 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { CarouselData } from "./CarouselData";
 import {FaChevronLeft,FaChevronRight} from 'react-icons/fa'
 import { Link } from "react-router-dom";
-import { TransitionGroup, CSSTransition} from 'react-transition-group';
-const Carousel = () => {
-    const [current, setCurrent] = useState(0)
-    const length = CarouselData.length
-    
-    const nextSlide = () => {
-        setCurrent(current === length -1 ? 0 : current + 1)
+
+const colors = ["#0088FE", "#00C49F", "#FFBB28"];
+const delay = 15000;
+
+function Slideshow() {
+  const [index, setIndex] = useState(0);
+  const timeoutRef = useRef(null);
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
     }
+  }
 
-    const prevSlide = () => {
-        setCurrent(current === 0 ? length -1 : current - 1)
-    }
+  useEffect(() => {
+      resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === colors.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
 
-    if (!Array.isArray(CarouselData) || length <= 0) {
-        return null;
-    }
+    return () => {
+        resetTimeout()
+    };
+  }, [index]);
 
-    return(
-        <section>
-            {/* mapage des données */}
-                {CarouselData.map((slide,index) => {
-                    return (
-                        <div key={index} className={`${index === current ? "w-screen h-screen-40 md:h-screen-40 sm:h-screen-50 relative" : "hidden"}`}>
-                            {/* ceci est l'image */}
-                            <Link to={slide.link} >
-                                <TransitionGroup>
-                                    <CSSTransition
-                                        key={current}
-                                        timeout={1000}
-                                        classNames='slide-right'
-                                    >
-                                        <img src={slide.image} alt="produit" className="w-full h-full object-bottom object-cover"/>
-                                    </CSSTransition>
-                                </TransitionGroup>
-                            </Link>
-                                    {/* les deux cheuvrons servant à manipuler le carousel */}
-                                    <FaChevronLeft className="absolute inset-y-1/2 left-6 sm:left-1 md:left-3 font text-5xl text-black z-20 cursor-pointer select-none opacity-40" onClick={prevSlide} />
-                                    <FaChevronRight className="absolute inset-y-1/2 right-6 sm:right-1 md:right-3 font text-5xl text-black z-20 cursor-pointer select-none opacity-40" onClick={nextSlide}/>
-                                
-                                {/* la div contenant les infos et le lien correspondant à l'image */}
-                                <div className={index === current 
-                                    ? "opacity-1 transition duration-1000 absolute sm:static sm:p-10 sm:hidden top-20 left-20 text-center" 
-                                    : "opacity-0"} key={index}>
-                                    <p className="text-3xl sm:text-blue-400 text-yellow-400 text-center">
-                                    { index === current && slide.text}
-                                    </p>
-                                    <Link to={slide.link} >  
-                                        <button className="sm:bg-nav-blueClar bg-yellow-400 hover:bg-yellow-500 rounded-full px-7 py-2 mt-2 sm:mt-4">
-                                            Voir plus
-                                        </button>
-                                    </Link>
-                                </div>
-                            </div>  
-                       
+  return (
+    <div className="slideshow w-full">
+      <div
+        className="slideshowSlider"
+        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+      >
+        {CarouselData.map((slide, index) => (
+          <div
+            className="slide"
+            key={index}
+          >
+              <img src={slide.image} />
+          </div>
+        ))}
+      </div>
 
-                    )
-                })}
-       </section>
-    )
-};
-export default Carousel
+      <div className="slideshowDots">
+        {CarouselData.map((_, idx) => (
+          <div key={idx}
+          className={`slideshowDot${index === idx ? " active" : ""}`}
+          onClick={() => {
+            setIndex(idx);
+          }}
+           ></div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Slideshow
--- src/app/views/HomeView.jsx
+++ src/app/views/HomeView.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 // import { ROLE_ADMIN } from './../shared/constants/rolesConstant';
 // import { URL_ADMIN_HOME } from './../shared/constants/urls/urlConstants';
 import NewProduct from '../components/homepage/NewProduct';
-import  Carousel  from '../components/layouts/carousel/Carousel';
+import Slideshow from '../components/layouts/carousel/Carousel';
 const HomeView = ({ history }) => {
     return (
         <div>
-            <Carousel />            
+            <Slideshow />            
             <div className="w-11/12 mx-auto my-3">
                 <NewProduct />      
                 {/* { hasRole(ROLE_ADMIN) && (
