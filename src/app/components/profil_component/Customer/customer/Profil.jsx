a6b1f43cc4cfb655a19f308d0e5995bf402dbc70
Rémi Willebrouck <willremi@gmail.com>
2022-01-06 12:26:29 +0100

    styliser formulaire

--- src/app/components/profil_component/Customer/customer/Profil.jsx
+++ src/app/components/profil_component/Customer/customer/Profil.jsx
@@ -16,53 +16,60 @@ const ProfilUser = () => {
         phone: user.phone === null ? '' : user.phone
     }
 
-    
+
     const handleEdit = (values) => {
         const userId = user.userId;
         editProfil(userId, values).then(res => {
             console.log(res.data.message)
-            
+
         })
-        .catch((error) => console.log(error))
+            .catch((error) => console.log(error))
     }
 
     return (
         <>
-            <h2 className='text-nav-blue font-semibold uppercase text-xl'>Données personnelles</h2>
+            <h2 className='text-nav-blue font-bold uppercase text-xl mb-8'>Données personnelles</h2>
 
             <Formik initialValues={valuesProfil} validationSchema={EditUserSchema} onSubmit={handleEdit}>
                 <Form>
-                    <div className="flex space-x-2">
-                        <p>Civilité : </p>
+                    {/* genre */}
+                    <div className="flex space-x-2 mb-4">
+                        <p className='text-nav-blue font-semibold text-lg'>Civilité : </p>
                         <label>
-                            <Field type="radio" name="civility" value="Man" />
-                            Mr
+                            <Field type="radio" name="civility" value="Man" className="ml-3  mr-2" />
+                            Monsieur
                         </label>
                         <label>
                             <Field
                                 type="radio"
                                 name="civility"
                                 value="Woman"
+                                className="ml-3 mr-2"
                             />
-                            Mme
+                            Madame
                         </label>
                         <label>
                             <Field
                                 type="radio"
                                 name="civility"
                                 value="Other"
+                                className="ml-3 mr-2"
                             />
                             Autre
                         </label>
                     </div>
 
+                    {/* Nom et Prénom */}
                     <div className='flex'>
-                        <Field type="text" name="lastName" component={CustomInput} />
-                        <Field type="text" name="firstName" component={CustomInput} />
+                        <Field type="text" name="lastName" component={CustomInput} placeholder="Nom" className="mr-2 border-b-4 border-r-4  border-gray-300" />
+                        <Field type="text" name="firstName" component={CustomInput} placeholder="Prénom" className="ml-2 border-b-4 border-r-4  border-gray-300" />
                     </div>
-                    <Field type="text" name="email" component={CustomInput} />
-                    <Field type="date" name="birthday_date" component={CustomInput} />
-                    <Field type="text" name="phone" component={CustomInput} />
+
+                    {/* Email/Date de naissance/téléphone */}
+                    <Field type="text" name="email" component={CustomInput} placeholder="E-mail" className="my-4 border-b-4 border-r-4  border-gray-300" />
+                    <Field type="date" name="birthday_date" component={CustomInput} placeholder="Date de naissance" className="my-4 border-b-4 border-r-4  border-gray-300" />
+                    <Field type="text" name="phone" component={CustomInput} placeholder="Téléphone (optionnel)" className="my-4 border-b-4 border-r-4  border-gray-300" />
+
                     <button type="submit" className="btn btn-yellow mb-2">
                         Modifier
                     </button>
