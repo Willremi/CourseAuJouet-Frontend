e5efd60766d6e910f4a124d28291415d9f55f12e
Rémi Willebrouck <willremi@gmail.com>
2022-02-04 15:36:03 +0100

    fin du ticket pour test

--- src/app/api/backend/api.Backend.js
+++ src/app/api/backend/api.Backend.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-import handleHttpSuccess from "../../shared/components/form-and-error-components/HandleHttpSuccess";
+import handleHttpSuccess from "./../../shared/components/form-and-error-components/HandleHttpSuccess";
 import { getToken } from "../../shared/services/tokenServices";
 import handleHttpError from './../../shared/components/form-and-error-components/HandleHttpError';
+
 
 /**
  * Instance axios to the BACKEND 
