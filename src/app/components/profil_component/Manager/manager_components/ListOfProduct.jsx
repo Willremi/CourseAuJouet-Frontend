a17712fb715f89deb87ee4aaa955c8a4ff30b435
Rémi Willebrouck <willremi@gmail.com>
2022-02-07 17:03:45 +0100

    début stylisation pagination en brut dans listProduct

--- src/app/components/profil_component/Manager/manager_components/ListOfProduct.jsx
+++ src/app/components/profil_component/Manager/manager_components/ListOfProduct.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { deleteManyProducts, deleteProduct, getAllProducts } from "../../../../api/backend/product";
 import { Icon } from "@iconify/react";
 import { PlusIcon } from "@heroicons/react/solid";
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { URL_ADD_PRODUCT, URL_MODIFY_PRODUCT } from "../../../../shared/constants/urls/urlConstants";
 import { useDispatch } from "react-redux";
 import { setProductToChange } from "../../../../shared/redux-store/updateProductSlice";
@@ -17,26 +17,22 @@ const ListOfProduct = () => {
 
   const [actualPage, setActualPage] = useState(1)
 
-  let numberOfItems = 10
-  // let actualPage = 1
+  let numberOfItems = 12
   let maxPages = Math.ceil(product.length / numberOfItems)
 
-  function firstPage () {
+  function firstPage() {
     setActualPage(1)
   }
-  
+
   function previousPage() {
-    if(numberOfItems >= 0) {
+    if (numberOfItems >= 0) {
       setActualPage(actualPage > 1 ? actualPage - 1 : 1)
     }
   }
 
   function nextPage() {
-    if(numberOfItems<=product.length) {
-      // if(actualPage < maxPages) {
-      //   setActualPage(actualPage + 1)
-      // }
-      setActualPage(actualPage < maxPages ? actualPage + 1 : maxPages) 
+    if (numberOfItems <= product.length) {
+      setActualPage(actualPage < maxPages ? actualPage + 1 : maxPages)
     }
   }
 
@@ -51,24 +47,24 @@ const ListOfProduct = () => {
       .catch((error) => console.log(error));
   }, []);
 
-  
-  
-  function setProductToModify(produit){
+
+
+  function setProductToModify(produit) {
     dispatch(setProductToChange(produit))
   }
 
-  function deleteOneProduct(productToDelete){
+  function deleteOneProduct(productToDelete) {
     deleteProduct(productToDelete)
-    setProduct(product.filter(item =>  item._id !== productToDelete._id))
+    setProduct(product.filter(item => item._id !== productToDelete._id))
   }
 
-  function deleteOneSelectedProduct(productToDelete){
+  function deleteOneSelectedProduct(productToDelete) {
     deleteProduct(productToDelete)
-    setSelectedProduct(selectedProduct.filter(item =>  item._id !== productToDelete._id))
-    setProduct(product.filter(item =>  item._id !== productToDelete._id))
+    setSelectedProduct(selectedProduct.filter(item => item._id !== productToDelete._id))
+    setProduct(product.filter(item => item._id !== productToDelete._id))
   }
 
-  function deleteList(){
+  function deleteList() {
     deleteManyProducts(selectedProduct)
     setSelectedProduct([])
   }
@@ -78,75 +74,75 @@ const ListOfProduct = () => {
         className="flex flex-col justify-end 
       sm:justify-center"
       >
-        {selectedProduct.length > 0 ? 
-        <div>
-          <table className=" w-full ">
-        <thead>
-          <tr className="flex text-left font-semibold w-full border-2 border-gray-400 bg-white rounded-full my-1">
-            <th className="flex w-5/12 py-1 px-5 sm:hidden">Nominations</th>
-            <th className="flex w-3/12 ml-5">Références</th>
-            <th className="flex w-3/12 ">Date</th>
-            <th className="flex w-1/12 ">Prix</th>
-            <th className="flex w-2/12 ">Stock</th>
-            <th className="flex w-1/12 ">Actions</th>
-          </tr>
-        </thead>
-        <tbody className=" bg-white">
-          {selectedProduct.map((product, index) => (
-            <tr
-              key={index}
-              className="flex justify-between w-full items-center first:rounded-t-xl first:border-t-2 last:rounded-b-xl last:border-b-2 border-r-2 border-l-2 border-gray-400"
+        {selectedProduct.length > 0 ?
+          <div>
+            <table className=" w-full ">
+              <thead>
+                <tr className="flex text-left font-semibold w-full border-2 border-gray-400 bg-white rounded-full my-1">
+                  <th className="flex w-5/12 py-1 px-5 sm:hidden">Nominations</th>
+                  <th className="flex w-3/12 ml-5">Références</th>
+                  <th className="flex w-3/12 ">Date</th>
+                  <th className="flex w-1/12 ">Prix</th>
+                  <th className="flex w-2/12 ">Stock</th>
+                  <th className="flex w-1/12 ">Actions</th>
+                </tr>
+              </thead>
+              <tbody className=" bg-white">
+                {selectedProduct.map((product, index) => (
+                  <tr
+                    key={index}
+                    className="flex justify-between w-full items-center first:rounded-t-xl first:border-t-2 last:rounded-b-xl last:border-b-2 border-r-2 border-l-2 border-gray-400"
+                  >
+                    <td className="flex w-5/12 py-1 pl-5 truncate sm:hidden">
+                      {product.product_name}
+                    </td>
+                    <td className="flex w-3/12 ml-5">{product.reference}</td>
+                    <td className="flex w-3/12 ">
+                      {onSaleDate(product.on_sale_date)}
+                    </td>
+                    <td className="flex w-1/12 ">{product.price / 100}€</td>
+                    <td className="flex w-2/12 ">
+                      <input
+                        type="text"
+                        placeholder={product.stock}
+                        className="w-2/3 h-8"
+                      />
+                      <button>
+                        <Icon
+                          icon="dashicons:update-alt"
+                          className="scale-200 text-nav-blue mx-3"
+                        />
+                      </button>
+                    </td>
+                    <td className="flex flex-row justify-around pr-3 w-1/12">
+                      <button>
+                        <Icon
+                          icon="carbon:change-catalog"
+                          className="scale-150 text-nav-blue"
+                        />
+                      </button>
+                      <button onClick={() => deleteOneSelectedProduct(product)}>
+                        <Icon
+                          icon="bx:bxs-trash"
+                          className="scale-150 text-nav-blue"
+                        />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <button
+              type="button"
+              className="flex flex-row font-semibold text-lg text-darkblue-100 items-center"
+              onClick={() => deleteList()}
             >
-              <td className="flex w-5/12 py-1 pl-5 truncate sm:hidden">
-                {product.product_name}
-              </td>
-              <td className="flex w-3/12 ml-5">{product.reference}</td>
-              <td className="flex w-3/12 ">
-                {onSaleDate(product.on_sale_date)}
-              </td>
-              <td className="flex w-1/12 ">{product.price / 100}€</td>
-              <td className="flex w-2/12 ">
-                <input
-                  type="text"
-                  placeholder={product.stock}
-                  className="w-2/3 h-8"
-                />
-                <button>
-                  <Icon
-                    icon="dashicons:update-alt"
-                    className="scale-200 text-nav-blue mx-3"
-                  />
-                </button>
-              </td>
-              <td className="flex flex-row justify-around pr-3 w-1/12">
-                <button>
-                  <Icon
-                    icon="carbon:change-catalog"
-                    className="scale-150 text-nav-blue"
-                  />
-                </button>
-                <button onClick={() => deleteOneSelectedProduct(product)}>
-                  <Icon
-                    icon="bx:bxs-trash"
-                    className="scale-150 text-nav-blue"
-                  />
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <button
-          type="button"
-          className="flex flex-row font-semibold text-lg text-darkblue-100 items-center"
-          onClick={() => deleteList()}
-        >
-          <Icon icon="ci:trash-full" className="w-6 h-6 mr-2 text-nav-blue"/>
-          Supprimer les produits
-        </button>
-        </div> 
-        : null}
-        
+              <Icon icon="ci:trash-full" className="w-6 h-6 mr-2 text-nav-blue" />
+              Supprimer les produits
+            </button>
+          </div>
+          : null}
+
       </div>
 
       <h2
@@ -192,7 +188,7 @@ const ListOfProduct = () => {
                       setSelectedProduct([...selectedProduct, product]);
                     } else {
                       let i = selectedProduct.indexOf(product)
-                      selectedProduct.splice( i , 1)
+                      selectedProduct.splice(i, 1)
                       setSelectedProduct([...selectedProduct])
                     }
                   }}
@@ -220,15 +216,15 @@ const ListOfProduct = () => {
                 </button>
               </td>
               <td className="flex flex-row justify-around pr-3 w-1/12">
-                  <Link to={URL_MODIFY_PRODUCT}>            
-                    <button onClick={() =>setProductToModify(product) }>
-                      <Icon
-                        icon="carbon:change-catalog"
-                        className="scale-150 text-nav-blue"
-                      />
-                    </button>
-                  </Link>
-                <button onClick={()=> deleteOneProduct(product)}>
+                <Link to={URL_MODIFY_PRODUCT}>
+                  <button onClick={() => setProductToModify(product)}>
+                    <Icon
+                      icon="carbon:change-catalog"
+                      className="scale-150 text-nav-blue"
+                    />
+                  </button>
+                </Link>
+                <button onClick={() => deleteOneProduct(product)}>
                   <Icon
                     icon="bx:bxs-trash"
                     className="scale-150 text-nav-blue"
@@ -240,11 +236,25 @@ const ListOfProduct = () => {
         </tbody>
       </table>
 
-      <button onClick={firstPage}>Première page</button>
-      <button onClick={previousPage}>précédent</button>
-      <span>/ {actualPage} - {maxPages} /</span>
-      <button onClick={nextPage}>suivant</button>
-      <button onClick={lastPage}>Dernière page</button>
+      {/* pagination */}
+      <div className="mt-4 flex justify-around mx-96">
+        <button className="border-2 border-nav-blueClar p-2 shadow-xl rounded-l-xl" onClick={firstPage}>
+          <Icon icon="fluent:arrow-previous-24-filled" />
+        </button>
+
+        <button className="border-t-2 border-b-2 border-nav-blueClar shadow-xl text-center p-2 text-lg md:text-lg xl:text-xl" onClick={previousPage}>
+          <Icon icon="grommet-icons:previous" />
+        </button>
+        
+        <span className="border-2 border-nav-blueClar shadow-xl text-center p-2 text-lg md:text-lg xl:text-xl">{actualPage} / {maxPages}</span>
+
+        <button className="border-t-2 border-b-2 border-nav-blueClar shadow-xl text-center p-2 text-lg md:text-lg xl:text-xl" onClick={nextPage}>
+          <Icon icon="grommet-icons:next" />
+        </button>
+        <button className="border-2 border-nav-blueClar p-2 shadow-xl rounded-r-xl" onClick={lastPage}>
+          <Icon icon="fluent:arrow-next-24-filled" />
+        </button>
+      </div>
     </>
   );
 };
