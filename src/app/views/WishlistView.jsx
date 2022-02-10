8c874f764c7b194ffa776f8b3cbc05f42d6edcde
Adrien Boudet <adrien.boudet1@gmail.com>
2022-02-10 09:38:42 +0100

    commit avant checkout

--- src/app/views/WishlistView.jsx
+++ src/app/views/WishlistView.jsx
@@ -1,7 +1,20 @@
+import React ,{ useState } from "react"
+import { useEffect } from "react"
+import { useSelector } from "react-redux"
+import { getOneProduct } from "../api/backend/product"
+
 const WishlistView = () => {
+    const [wishlist, setWishlist ] = useState()
+    const selectWishlist = useSelector()
+    
+    useEffect(() => {
+        getOneProduct(selectWishlist._id)
+            .then( (res) => setWishlist(res.data))
+    },[])
+    
     return (
         <div>
-            ceci est la page Wishlist
+            
         </div>
     )
 }
